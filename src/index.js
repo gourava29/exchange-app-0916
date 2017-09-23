adef1f8acb05c94eeac49dcfa06ab4bcb31815a0
jayesh <sisodiya.jayesh@gmail.com>
2017-09-23 19:49:33 +0800

    Fixed user img path and added font-awesome

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './AdminLTE.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <Routes history={browserHistory} />,
